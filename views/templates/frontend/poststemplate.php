c6ad029582528bb385ec1065cc839798ab88fc9c
Yanou <maxilarix1@gmail.com>
2020-06-22 12:26:55 +0200

    Create poststemplate.php

---
+++ views/templates/frontend/poststemplate.php
@@ -0,0 +1,7 @@
+<div class="col-lg-4 p-0 px-lg-3 pt-3">
+    <h3 class="accent-color"><?=$this->_title?></h3>
+    <p><?=$this->_content?></p>
+    <p class="text-right">
+        <a class="text-right" href="/?id=<?=$this->_postId?>&amp;action=Chapitre">lire plus</a>
+    </p>
+</div>
